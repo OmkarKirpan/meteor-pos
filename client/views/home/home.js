f51d0521877296f9ecc6cb0997a7b38a85bc1b3b
Leo Matthew <leo@metronconsulting.com>
2015-09-14 12:00:57 +0530

    index page created

--- client/views/home/home.js
+++ client/views/home/home.js
@@ -1,7 +1,27 @@
 Template.home.events(
 {
+ 
  'click #btnAdd': function(){
-    alert("You clicked btnAdd");
-}
-
+      res = '<div><div style="display: inline-block;" width="80%">'+$("#ProductID").val()+'</div>';
+      res = res + '<div width="80%" style="display: inline-block;">'+$("#ProductName").val()+'</div>';
+      res = res + '<div width="80%" style="display: inline-block;">'+$("#Quantity").val()+'</div>';
+      res = res + '<div width="80%" style="display: inline-block;">'+$("#Measurement").val()+'</div></div>';
+      $("#Container").append(res);
+      $("#ProductID").focus();
+},
+'keypress #ProductID': function (evt, template) {
+    if (evt.which == 13) {
+      $("#ProductName").focus();
+    }
+  },
+'keypress #ProductName': function (evt, template) {
+    if (evt.which == 13) {
+      $("#Quantity").focus();
+    }
+  },
+'keypress #Quantity': function (evt, template) {
+    if (evt.which == 13) {
+      $("#btnAdd").focus();
+    }
+  }
 });
